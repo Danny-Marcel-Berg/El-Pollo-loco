0b49974a54b091d5557621acd2e97ea98943e080
Danny-Marcel-Berge <danny.berg@outlook.de>
2024-04-14 23:49:36 +0200

    first commit

---
+++ js/game.js
@@ -0,0 +1,308 @@
+let canvas;
+let world;
+let keyboard = new Keyboard();
+let isLoading = false;
+let isMuted = false;
+let fullscreen = false;
+const keyMapping = {
+    39: "RIGHT",
+    37: "LEFT",
+    38: "UP",
+    40: "DOWN",
+    32: "SPACE",
+    68: "D"
+};
+
+document.addEventListener('fullscreenchange', exitHandler);
+document.addEventListener('webkitfullscreenchange', exitHandler);
+document.addEventListener('mozfullscreenchange', exitHandler);
+document.addEventListener('MSFullscreenChange', exitHandler);
+
+
+window.addEventListener("keydown", (e) => {
+    if(keyMapping[e.keyCode]) {
+        keyboard[keyMapping[e.keyCode]] = true;
+    }
+});
+
+
+window.addEventListener("keyup", (e) => {
+    if(keyMapping[e.keyCode]) {
+        keyboard[keyMapping[e.keyCode]] = false;
+    }
+});
+
+
+/**
+ * initialize mobile press events
+ * 
+ */
+function initMobile() {
+    mobileKeyPressEvents();
+    mobileKeyReleaseEvents();
+}
+
+
+/**
+ * starts the game 
+ * 
+ */
+async function startGame() {
+    document.getElementById("startScreen").classList.add("d-none");
+    document.getElementById("loading").classList.remove("d-none");
+    if (!isLoading) {
+        await newGame();
+        setTimeout(() => {
+            isLoading = true;
+            showGame();
+        }, 3000);
+    }
+}
+
+
+/**
+ * sets a new World in the canvas
+ * 
+ */
+async function newGame() {
+    canvas = document.getElementById("canvas");
+    initLevel();
+    world = new World(canvas, keyboard);
+}
+
+
+/**
+ * removes the loading view and shows the game
+ * 
+ */
+function showGame() {
+    document.getElementById("loading").classList.add("d-none");
+    document.getElementById("canvasContent").classList.remove("d-none");
+    document.getElementById("leftMobile-container").classList.remove("d-none");
+    document.getElementById("rightMobile-container").classList.remove("d-none");
+
+}
+
+
+/**
+ * shows the win screen
+ * 
+ */
+function gameOverWin() {
+    document.getElementById("gameOverWin").classList.remove("d-none");
+}
+
+
+/**
+ * shows the lose screen
+ * 
+ */
+function gameOverLose() {
+    document.getElementById("gameOverLose").classList.remove("d-none");
+}
+
+
+/**
+ * restarts the game
+ * 
+ */
+function restartGame() {
+    document.getElementById('gameOverWin').classList.add("d-none");
+    document.getElementById('gameOverLose').classList.add("d-none");
+    document.getElementById('loading').classList.remove("d-none");
+    document.getElementById('leftMobile-container').classList.add("d-none");
+    document.getElementById('rightMobile-container').classList.add("d-none");
+    newGame();
+    setTimeout(() => {
+        document.getElementById('loading').classList.add("d-none");
+        document.getElementById('leftMobile-container').classList.remove("d-none");
+        document.getElementById('rightMobile-container').classList.remove("d-none");
+    }, 1000);
+}
+
+
+/**
+ * toggle between mute and unmute
+ * 
+ */
+function toggleMute() {
+    let audioImage = document.getElementById("audio");
+      if (isMuted) {
+        audioImage.src = 'img/10_extras/ton-an.png';
+      } 
+      else {
+        audioImage.src = 'img/10_extras/ton-aus.png';
+      }
+      isMuted = !isMuted;
+}
+
+
+/**
+ * toggle between fullscreen and normal screen
+ * 
+ */
+function toggleFullscreen() {
+    let content = document.getElementById("content");
+    let fullscreenImage = document.getElementById("fullscreen");
+    if (fullscreen) {
+        fullscreenImage.src = 'img/10_extras/vollbild.png';
+        exitFullscreen();
+        content.classList.remove('fullscreen-mode');
+    }
+    else {
+        fullscreenImage.src = 'img/10_extras/minimieren.png';
+        enterFullscreen(document.getElementById("content"));
+        content.classList.add('fullscreen-mode');
+    }
+    fullscreen = !fullscreen;
+}
+
+
+/**
+ * open the fullscreen
+ * 
+ * @param {object} element 
+ */
+function enterFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
+}
+
+
+/**
+ * close the fullscreen
+ * 
+ */
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
+
+
+/**
+ * switches the fullscreen icons after pressing escape in fullscreen and sets fullscreen to false
+ * 
+ */
+function exitHandler() {
+    let content = document.getElementById("content");
+    if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
+        document.getElementById("fullscreen").src = 'img/10_extras/vollbild.png';
+        content.classList.remove('fullscreen-mode');
+        fullscreen = false;
+    }
+}
+
+
+/**
+ * show help page
+ * 
+ */
+function showHelpPage() {
+    let help = document.getElementById('help');
+    help.classList.remove('d-none');
+    help.innerHTML = showHelpFirstPage();
+}
+
+
+/**
+ * close help page
+ * 
+ */
+function closeHelpPage() {
+    let help = document.getElementById('help');
+    help.classList.add('d-none');
+}
+
+
+/**
+ * show first help page
+ * 
+ */
+function showFirstPage() {
+    let help = document.getElementById('help');
+    help.innerHTML = showHelpFirstPage();
+}
+
+
+/**
+ * show second help page
+ * 
+ */
+function showSecondPage() {
+    let help = document.getElementById('help');
+    help.innerHTML = showHelpSecondPage();
+}
+
+
+/**
+ * show third help page
+ * 
+ */
+function showThirdPage() {
+    let help = document.getElementById('help');
+    help.innerHTML = showHelpThirdPage();
+}
+
+/**
+ * sets the key to true, after pressing them (mobile)
+ * 
+ */
+function mobileKeyPressEvents() {
+    const leftButton = document.getElementById('left_button');
+    leftButton.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        keyboard.LEFT = true;
+    });
+    const rightButton = document.getElementById('right_button');
+    rightButton.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        keyboard.RIGHT = true;
+    });
+    const jump = document.getElementById('jump_button');
+    jump.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        keyboard.SPACE = true;
+    });
+    const throwBottle = document.getElementById('throw_button');
+    throwBottle.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        keyboard.D = true;
+    });
+}
+
+
+/**
+ * This function sets the key to false, after releasing them (mobile)
+ */
+function mobileKeyReleaseEvents() {
+    const leftButton = document.getElementById('left_button');
+    leftButton.addEventListener('touchend', () => {
+        keyboard.LEFT = false;
+    });
+    const rightButton = document.getElementById('right_button');
+    rightButton.addEventListener('touchend', () => {
+        keyboard.RIGHT = false;
+    });
+    const jump = document.getElementById('jump_button');
+    jump.addEventListener('touchend', (e) => {
+        keyboard.SPACE = false;
+    });
+    const throwBottle = document.getElementById('throw_button');
+    throwBottle.addEventListener('touchend', (e) => {
+        keyboard.D = false;
+    });
+}
---
+++ models/finalBoss.class.js
@@ -0,0 +1,207 @@
+class FinalBoss extends MoveableObject {
+
+    width = 200;
+    height = 400;
+    y = 0;
+    i = 0;
+
+    IMAGES_WALK = [
+        'img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img/4_enemie_boss_chicken/1_walk/G4.png',
+    ];
+
+    IMAGES_ALERT = [
+        'img/4_enemie_boss_chicken/2_alert/G5.png',
+        'img/4_enemie_boss_chicken/2_alert/G6.png',
+        'img/4_enemie_boss_chicken/2_alert/G7.png',
+        'img/4_enemie_boss_chicken/2_alert/G8.png',
+        'img/4_enemie_boss_chicken/2_alert/G9.png',
+        'img/4_enemie_boss_chicken/2_alert/G10.png',
+        'img/4_enemie_boss_chicken/2_alert/G11.png',
+        'img/4_enemie_boss_chicken/2_alert/G12.png',
+    ];
+
+    IMAGES_ATTACK = [
+        'img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img/4_enemie_boss_chicken/3_attack/G15.png',
+        'img/4_enemie_boss_chicken/3_attack/G16.png',
+        'img/4_enemie_boss_chicken/3_attack/G17.png',
+        'img/4_enemie_boss_chicken/3_attack/G18.png',
+        'img/4_enemie_boss_chicken/3_attack/G19.png',
+        'img/4_enemie_boss_chicken/3_attack/G20.png',
+    ];
+
+    IMAGES_HURT = [
+        'img/4_enemie_boss_chicken/4_hurt/G21.png',
+        'img/4_enemie_boss_chicken/4_hurt/G22.png',
+        'img/4_enemie_boss_chicken/4_hurt/G23.png',
+    ];
+
+    IMAGES_DEAD = [
+        'img/4_enemie_boss_chicken/5_dead/G24.png',
+        'img/4_enemie_boss_chicken/5_dead/G25.png',
+        'img/4_enemie_boss_chicken/5_dead/G26.png',
+    ];
+
+    hadFirstContact = false;
+
+    constructor() {
+        super().loadImage(this.IMAGES_ALERT[0]);
+        this.loadImages(this.IMAGES_WALK);
+        this.loadImages(this.IMAGES_ALERT);
+        this.loadImages(this.IMAGES_ATTACK);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
+        this.speed = 2;
+        this.x = 2000;
+        this.animate();
+    }
+
+
+    /**
+     * drains energy from the target
+     * 
+     */
+    hitFinalBoss() {
+        this.energyFinalBoss -= 10;    
+        if (this.energyFinalBoss < 0) {
+            this.energyFinalBoss = 0;
+        }
+        else {
+            this.lastHitFinalBoss = new Date().getTime();
+        }
+    }
+
+
+    /**
+     * 
+     * @returns if the last hit was 1 second ago or not
+     */
+    isHurtFinalBoss() {
+        let timepassed = new Date().getTime() - this.lastHitFinalBoss;
+        timepassed = timepassed / 1000;
+        return timepassed < 1;
+    }
+
+
+    /**
+     * 
+     * @returns if the energy is 0 
+     */
+    isDeadFinalBoss() {
+        return this.energyFinalBoss == 0;
+    }
+
+
+    /**
+     * animation of the final boss
+     * 
+     */
+    finalBossAnnimation() {
+        if (this.isDeadFinalBoss()) {
+            this.finalBossAnnimationDead();
+        }
+        else if (this.isHurtFinalBoss()) {
+            this.finalBossAnnimationHurt();
+        }        
+        else if (this.i < 15) {
+            this.finalBossAnnimationAlert();     
+        }
+        else if (this.i < 30) {
+            this.finalBossAnnimationAttack();
+        }
+        else {        
+            this.finalBossAnnimationWalk();
+        }
+        this.i++;    
+        this.finalBossFirstContact();   
+    }
+
+
+    /**
+     * animation at death
+     * 
+     */
+    finalBossAnnimationDead() {
+        this.playAnnimation(this.IMAGES_DEAD);
+        world.gameOver = true;
+        world.background_music.pause()
+        setTimeout(() => {
+            this.clearAllIntervals();
+            this.playSound(world.win_sound);
+            gameOverWin();
+        }, 1500);
+    }
+
+
+    /**
+     * animation at hurt
+     * 
+     */
+    finalBossAnnimationHurt() {
+        this.playAnnimation(this.IMAGES_HURT);
+    }
+
+
+    /**
+     * animation at alert
+     * 
+     */
+    finalBossAnnimationAlert() {
+        this.playAnnimation(this.IMAGES_ALERT); 
+    }
+
+
+    /**
+     * animation at attack
+     * 
+     */
+    finalBossAnnimationAttack() {
+        this.playAnnimation(this.IMAGES_ATTACK);
+    }
+    
+
+    /**
+     * animation at walking
+     * 
+     */
+    finalBossAnnimationWalk() {
+        this.playAnnimation(this.IMAGES_WALK);
+    }
+
+
+    /**
+     * checks if the character had first contact with the final boss
+     * 
+     */
+    finalBossFirstContact() {
+        if (world.character.x > 1425 && !this.hadFirstContact) {
+            this.i = 0;
+            this.hadFirstContact = true;
+        }
+    }
+
+
+    /**
+     * animates the final boss
+     * 
+     */
+    animate() {
+        setInterval(() => {
+            this.finalBossAnnimation();            
+        }, 200);
+        setInterval(() => {
+            if (this.hadFirstContact && this.i > 30 && !this.isDeadFinalBoss() && !this.isHurtFinalBoss()) {
+                this.x -= this.speed;    
+            }        
+        }, 1000 / 60);
+        setInterval(() => {
+            if (this.isHurtFinalBoss()) {
+                this.playSound(world.finalbossHurt_sound);
+            }
+        }, 100);
+    }
+}
